48ed3238771f8e6117c25ada0e00fd9f3328b0f7
teipirbus <panosipath@aol.com>
2013-02-10 18:52:56 +0200

    Final
    Final

---
+++ Εργασια - Code/src/javaapplication1/JavaApplication4.java
@@ -0,0 +1,10 @@
+package javaapplication1;
+
+public class JavaApplication4 {
+    
+    
+    public static void main(String[] args) {
+      Epilogi frame = new Epilogi();  
+      frame.setVisible(true);
+    }
+}
